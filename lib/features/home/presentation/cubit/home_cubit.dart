0a36b9b5e0789f98f726c951b02c5abe9f54b46b
KarimTamer74 <kemotamer448@gmail.com>
2025-11-02 14:01:47 +0200

    use get_all_news of remote data source to handle states

--- lib/features/home/presentation/cubit/home_cubit.dart
+++ lib/features/home/presentation/cubit/home_cubit.dart
@@ -2,11 +2,13 @@ import 'dart:developer';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:news_app/features/home/data/data_source/remote_data_source.dart';
+import 'package:news_app/features/home/data/models/article_model.dart';
 import 'package:news_app/features/home/presentation/cubit/home_states.dart';
 
 class HomeCubit extends Cubit<HomeState> {
   HomeCubit() : super(HomeStateInitial());
   final RemoteDataSource remoteDataSource = RemoteDataSource();
+  List<ArticleModel> articles = [];
   Future<void> getTrendingNews() async {
     emit(GetTrendingNewsLoading());
     await remoteDataSource.getTrendingNews().then(
@@ -19,5 +21,35 @@ class HomeCubit extends Cubit<HomeState> {
         emit(GetTrendingNewsFailure());
       },
     );
+  }
+
+  Future<void> getAllNews() async {
+    emit(GetAllNewsLoading());
+    await remoteDataSource.getTrendingNews().then(
+      (value) {
+        log("Articles: $value", name: 'Get Trending News');
+        articles = value;
+        emit(GetAllNewsLoaded(articles: value));
+      },
+      onError: (error) {
+        log("Error $error", name: "Get Trending News");
+        emit(GetAllNewsFailure());
+      },
+    );
+  }
+
+  List<ArticleModel> searchForNews(String query) {
+    final List<ArticleModel> filteredList = [];
+    log("Query: $query");
+    log("Articls: $articles");
+    for (var element in articles) {
+      if (element.title.toLowerCase().contains(query.toLowerCase())) {
+        filteredList.add(element);
+      }
+    }
+    log("Filtered lenght ${filteredList.length}");
+    emit(GetAllNewsLoaded(articles: filteredList));
+
+    return filteredList;
   }
 }
