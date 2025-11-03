5b3690f614e2c916416aadcc52d0358582753522
KarimTamer74 <kemotamer448@gmail.com>
2025-11-03 22:09:52 +0200

    use bloc builder to handle all news states

--- lib/features/home/presentation/widgets/all_news_section.dart
+++ lib/features/home/presentation/widgets/all_news_section.dart
@@ -1,91 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app/core/shared_widgets/error_widget.dart';
+import 'package:news_app/core/shared_widgets/loading_indicator_widget.dart';
 import 'package:news_app/core/utils/app_colors.dart';
+import 'package:news_app/features/home/data/models/article_model.dart';
+import 'package:news_app/features/home/presentation/cubit/home_cubit.dart';
+import 'package:news_app/features/home/presentation/cubit/home_states.dart';
+import 'package:news_app/features/home/presentation/widgets/all_news_item_widget.dart';
 
 class AllNewsSection extends StatelessWidget {
   const AllNewsSection({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(12.0),
-      child: ListView.builder(
-        itemCount: 5,
-        itemBuilder: (context, index) {
+    return BlocBuilder<HomeCubit, HomeState>(
+      buildWhen: (previous, current) =>
+          current is GetAllNewsFailure ||
+          current is GetAllNewsLoading ||
+          current is GetAllNewsLoaded,
+      builder: (context, state) {
+        if (state is GetAllNewsLoading) {
+          return LoadingIndicatorWidget();
+        } else if (state is GetAllNewsFailure) {
+          return CustomErrorWidget();
+        } else if (state is GetAllNewsLoaded) {
+          final List<ArticleModel> allNewsArticles = state.articles;
           return Padding(
-            padding: const EdgeInsets.only(bottom: 20),
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                spacing: 15,
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadiusGeometry.circular(15),
-                    child: Image.asset(
-                      'assets/images/news.jpg',
-                      fit: BoxFit.cover,
-                      width: double.infinity,
+            padding: const EdgeInsets.all(12.0),
+            child: allNewsArticles.isEmpty
+                ? Center(
+                    child: Text(
+                      "No News Found",
+                      style: TextStyle(color: AppColors.white, fontSize: 28),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      spacing: 15,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Liberals are all mean girls now",
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                            color: AppColors.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.w700,
+                  )
+                : ListView.builder(
+                    itemCount: allNewsArticles.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.only(bottom: 20),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          child: AllNewsItemWidget(
+                            article: allNewsArticles[index],
                           ),
                         ),
-                        Text(
-                          "Most liberals these days are stereotypical mean girls. AOC's a prime example as she loves to belittle her fellow women. So much for feminism!",
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                            color: AppColors.grey,
-                            fontSize: 20,
-                            fontWeight: FontWeight.w700,
-                          ),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              "Washington Examiner",
-                              style: TextStyle(
-                                color: AppColors.blue,
-                                fontSize: 20,
-                                fontWeight: FontWeight.w700,
-                              ),
-                            ),
-                            Text(
-                              "2 days ago",
-                              style: TextStyle(
-                                color: AppColors.grey,
-                                fontSize: 20,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
+                      );
+                    },
                   ),
-                ],
-              ),
-            ),
           );
-        },
-      ),
+        } else {
+          return SizedBox.shrink();
+        }
+      },
     );
   }
 }
