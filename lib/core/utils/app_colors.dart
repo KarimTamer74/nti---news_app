fa9d8daa848b90c37b2b7baebc765f6a7dacc038
KarimTamer74 <kemotamer448@gmail.com>
2025-11-05 15:12:11 +0200

    add colors

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -8,4 +8,7 @@ class AppColors {
   static const Color white = Colors.white;
   static const Color red = Colors.red;
   static const Color grey = Colors.grey;
+    static const background = Color(0xFF0D0D0D);
+  static const card = Color(0xFF1E1E1E);
+  static const accent = Color(0xFF42A5F5);
 }
